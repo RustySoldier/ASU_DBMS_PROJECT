0bbd3e3906965272d6c122986531e721fa86da0d
Jill Patel <pateljill2288@gmail.com>
2024-12-05 22:30:09 -0700

    Create SP_LogAction.sql

---
+++ SP_LogAction.sql
@@ -0,0 +1,22 @@
+DROP PROCEDURE IF EXISTS LogAction;
+GO
+
+CREATE PROCEDURE LogAction
+    @Action CHAR(1),
+    @TableName VARCHAR(20),
+    @ID INT,
+    @AffectedColumns NVARCHAR(MAX),
+    @ActionDateTime DATETIME
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @NextAuditId INT;
+	SET @NextAuditId = COALESCE((SELECT MAX(AuditId) FROM Audit), 0) + 1;
+
+    INSERT INTO Audit(AuditId, Action, TableName, RecordID, AffectedColumns, ActionDateTime)
+    VALUES (@NextAuditId, @Action, @TableName, @ID, 
+            CASE WHEN @Action = 'U' THEN @AffectedColumns ELSE 'ALL' END, 
+            @ActionDateTime);
+
+END;
+GO
